ef4c8274a6f6311326ae675f5b7b303e4095bc57
Jelger van Zaane <j.d.van.zaane@vu.nl>
2019-10-10 16:23:18 +0200

    edit csv to use commas

--- data_raw/prepare_date.R
+++ data_raw/prepare_date.R
@@ -4,7 +4,7 @@ file_list <- list.files("data_raw", pattern = "^dates", full.names = TRUE)
 ## Load all files and process them
 dates_list <- purrr::map(file_list,
            process_dates_csv,
-           delim = ";",
+           delim = ",",
            format = "%d-%m-%Y") %>%
     setNames(stringr::str_extract(basename(file_list), "(?<=dates_).*(?=\\.csv)"))
 
